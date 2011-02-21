82fefbb443ee19ad0e693752fe6a2a278f9441c1
Jonathan Swartz <swartz@pobox.com>
2011-02-20 23:53:41 -0500

    implemented

--- lib/Dancer/Template/Mason2.pm
+++ lib/Dancer/Template/Mason2.pm
@@ -1,11 +1,37 @@
 package Dancer::Template::Mason2;
-use 5.008;
+use Dancer::Config 'setting';
+use FindBin;
+use Mason;
 use strict;
 use warnings;
+use base 'Dancer::Template::Abstract';
+
+my $_engine;
+my $root_dir;
+
+sub init {
+    my $self = shift;
+    my $config = $self->config || {};
+
+    $root_dir = $config->{comp_root} ||= setting('views') || $FindBin::Bin . '/views';
+    $config->{data_dir} ||= $FindBin::Bin . "/data";
+    $config->{autoextend_request_path} ||= [];
+
+    $_engine = Mason->new(%$config);
+}
+
+sub default_tmpl_ext { "m" }
+
+sub render {
+    my ( $self, $template, $tokens ) = @_;
+
+    $template =~ s/^\Q$root_dir//;    # cut the leading path
+
+    my $content = $_engine->run( $template, %$tokens )->output;
+    return $content;
+}
 
 1;
-
-__END__
 
 =pod
 
@@ -15,13 +41,53 @@ Dancer::Template::Mason2 - Mason 2.x wrapper for Dancer
 
 =head1 SYNOPSIS
 
-    use Dancer::Template::Mason2;
+  # in 'config.yml'
+  template: 'mason2'
+
+  # in the app
+ 
+  get '/foo', sub {
+    template 'foo' => {
+        title => 'bar'
+    };
+  };
+
+Then, on C<views/foo.m>:
+
+    <%args>
+    $.title
+    </%args>
+
+    <h1><% $.title %></h1>
+
+    <p>Mason says hi!</p>
 
 =head1 DESCRIPTION
 
-Dancer::Template::Mason2 provides
+This class is an interface between Dancer's template engine abstraction layer
+and the L<Mason 2.x|Mason> templating system.
+
+In order to use this engine, set the template to 'mason2' in the configuration
+file:
+
+    template: mason2
+
+=head1 CONFIGURATION
+
+Parameters can also be passed to C<< Mason->new >> via the configuration file,
+like so:
+
+    engines:
+        mason2:
+            data_dir: /path/to/data_dir
+
+C<comp_root>, if not specified, defaults to the C<views> configuration setting
+or, if it's undefined, to the C</views> subdirectory of the application.
+
+C<data_dir>, if not specified, defaults to the C</data> subdirectory of the
+application.
 
 =head1 SEE ALSO
 
-L<Some::Module>
+L<Dancer>, L<Mason>, L<Dancer::Template::Mason>
 
---
+++ t/02-basic.t
@@ -0,0 +1,26 @@
+#!perl
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use Test::More tests => 2;
+
+use Dancer::Template::Mason2;
+
+my $engine;
+eval { $engine = Dancer::Template::Mason2->new };
+is $@, '', "Dancer::Template::Mason engine created";
+
+my $template = join( "/", $Bin, 'views', 'index.m' );
+my $result = $engine->render(
+    $template,
+    {
+        var1 => 1,
+        var2 => 2,
+        foo  => 'one',
+        bar  => 'two',
+        baz  => 'three'
+    }
+);
+
+my $expected = 'this is var1="1" and var2=2' . "\n\nanother line\n\none two three\n";
+is $result, $expected, "processed a template given as a file name";
